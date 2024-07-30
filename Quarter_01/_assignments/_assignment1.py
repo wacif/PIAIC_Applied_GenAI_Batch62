92f255308d2d1d553ca816c7a03b5ccdca9129f6
Wasif Nawaz <nwasi1930@gmail.com>
2024-07-30 20:42:29 +0500

    assignment file added

---
+++ Quarter_01/_assignments/_assignment1.py
@@ -0,0 +1,13 @@
+#   1-Basic Printing
+print("Hello, World!")
+print("Wasif")
+
+#   2-Printing Multiple Items
+first_name = "Wasif"
+last_name = "Nawaz"
+print(f"{first_name} {last_name}")
+
+num1 = 1
+num2 = 2
+num3 = 3
+print(f"{num1}, {num2}, {num3}")
