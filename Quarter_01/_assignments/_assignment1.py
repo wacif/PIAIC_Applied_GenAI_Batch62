92d8999ed42fe4a270d4caff5dd963f9126401eb
Wasif Nawaz <nwasi1930@gmail.com>
2024-07-31 12:25:48 +0500

    final completion

--- Quarter_01/_assignments/_assignment1.py
+++ Quarter_01/_assignments/_assignment1.py
@@ -5,9 +5,37 @@ print("Wasif")
 #   2-Printing Multiple Items
 first_name = "Wasif"
 last_name = "Nawaz"
-print(f"{first_name} {last_name}")
+print(first_name, last_name)
 
 num1 = 1
 num2 = 2
 num3 = 3
-print(f"{num1}, {num2}, {num3}")
+print(num1, num2, num3)
+
+#   3-Printing Special Characters
+print("Hello \nWorld!")     # new line character
+print("Hello \tWorld!")     # tab character
+
+#   4-Using the special Parameters
+fruit_1 = "apple"
+fruit_2 = "banana"
+fruit_3 = "cherry"
+print(f"{fruit_1}, {fruit_2}, {fruit_3}")   # seperated by comma
+print(f"{num1}-{num2}-{num3}")  # seperated by hyphen
+
+#   5-Using the end parameter
+print("Hello","World!", end=" ")
+print(1,2, end="")
+
+#   6-Escape Characters
+print(f'He said, "He is okay."')
+print("This is a backslash:\\")
+
+#   7-Combining Text & Numbers
+my_age = 24
+print(f"I,m {my_age} years old.")
+print(f"I love {fruit_3}, and it is at number {num1} in my favorite list.")
+
+#   8-Basic Loop for printing
+for i in range(1, 6):
+    print(i)
