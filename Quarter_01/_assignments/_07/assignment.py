d8e3759cbfcbe35aac1d33130d6b1bb37a96e7ae
Wasif Nawaz <nwasi1930@gmail.com>
2024-08-29 12:55:00 +0500

    assignment 07

---
+++ Quarter_01/_assignments/_07/assignment.py
@@ -0,0 +1,7 @@
+#   This file contains the solution of 7th assignment 
+#   Python Programming Assignment: Number Exploration Tool
+
+user_name = input("Please enter your name.")
+
+def getting_numbers():
+    
